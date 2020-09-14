965b2f498266664543300d50fa2c9bd05ca14521
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-09-14 13:18:08 -0300

    Criando o projeto

---
+++ backend/src/test/java/com/mayinfor/mayinforpesquisa/MayconinforgamespesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayinfor.mayinforpesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayconinforgamespesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
