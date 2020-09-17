432ad25ff048f200742f1e9b684f47bca7b9a814
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-09-17 20:58:25 -0300

    Criando navegacao com rotas

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../../assets/logo.svg'; 
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Header = () => (
     <header className="main-header" >
         <Logo />
-        <div className="logo-text">
-            <span className="logo-text-1">MayconInforgames</span>
-            <span className="logo-text-2"> Pesquisa</span>
-        </div>
+        <Link to="/">
+            <div className="logo-text">
+                <span className="logo-text-1">MayconInforgames</span>
+                <span className="logo-text-2"> Pesquisa</span>
+            </div>
+        </Link>
     </header>
 );
 
